55e0d51c10a92b53d26de2e193d95b627135cdf9
Nimosteve88 <nimosteve2@gmail.com>
2024-06-04 12:28:55 +0100

    Implemented separate web servers
    Added new web servers that the raspberry pi can listen and also send data to

--- EIE_Work_Space/RaspberryPiTesting/ServerTest/raspjson.py
+++ EIE_Work_Space/RaspberryPiTesting/ServerTest/raspjson.py
@@ -1,36 +1,42 @@
 import urequests as requests
 import ujson as json
 import utime as time
+from machine import Timer
 
 # Replace with the IP address of the machine running the Flask app
-starttime = time.time()
-url = 'http://192.168.0.103:5000/power'
+url = 'http://192.168.194.92:5000/power'
+energyurl = 'http://192.168.194.92:5000/energy'
 
 # Send GET request to Flask app
 response = requests.get(url)
+energyresponse = requests.get(energyurl)
+
+
 
 # Check if the request was successful
 if response.status_code == 200:
     # Parse the JSON data
     data = response.json()
+    energydata = energyresponse.json()
     print('Data from Flask server:', data)
 
     # Modify the JSON data by incrementing the 'power_flywheel' and 'power_extracted' values by 0.1, round them to 1 decimal place
-    # Assuming 'data' is your dictionary
     data['power_flywheel'] = str(round(float(data['power_flywheel']) + 0.1, 1))
     data['power_extracted'] = str(round(float(data['power_extracted']) + 0.1, 1))
+    energydata['energy_reserve'] = str(round(float(energydata['energy_reserve']) + 0.1, 1))
 
     # Convert data back to JSON
     json_data = json.dumps(data)
+    json_energydata = json.dumps(energydata)
     
-    # Optionally, you can send the modified data back to the server or do something else with it
-    # For example, sending the modified data back to the server
+
+    # Send the modified data back to the server
     response = requests.post(url, data=json_data, headers={'Content-Type': 'application/json'})
-    print("Time taken: ", time.time()-starttime)
+    response = requests.post(energyurl, data=json_energydata, headers={'Content-Type': 'application/json'})
+
+
     if response.status_code == 200:
-        
         print('Data successfully sent to Flask server')
-        
     else:
         print('Failed to send data to Flask server')
 else:
