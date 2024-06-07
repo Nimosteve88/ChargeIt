5a7c11fad96e33f88bb42e5585c0382bd03e1a3d
Nimosteve88 <nimosteve2@gmail.com>
2024-06-07 10:29:31 +0100

    Updated LED Code to communicate with UI

--- EEE_Work_Space/LED wifi.py
+++ EEE_Work_Space/LED wifi.py
@@ -1,5 +1,49 @@
 from machine import Pin, I2C, ADC, PWM
 from PID import PID
+import urequests as requests 
+import network
+import utime
+
+
+##########################WIFI CONNECTION##########################
+ssid = 'SteveGalaxy'
+password = 'ChargeIt'
+
+# Set WiFi to station interface
+wlan = network.WLAN(network.STA_IF)
+# Activate the network interface
+wlan.active(True)
+
+# Check if already connected
+if not wlan.isconnected():
+    print('Connecting to network...')
+    wlan.connect(ssid, password)
+
+    max_wait = 10
+    # Wait for connection
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            # Connection successful
+            break
+        max_wait -= 1
+        print('Waiting for connection... ' + str(max_wait))
+        utime.sleep(1)
+
+    # Check connection
+    if wlan.status() != 3:
+        # No connection
+        raise RuntimeError('Network connection failed')
+else:
+    print('Already connected to network')
+
+# Connection successful
+print('WLAN connected')
+status = wlan.ifconfig()
+pico_ip = status[0]
+print('IP = ' + status[0])
+
+##########################WIFI CONNECTION##########################
+
 
 vret_pin = ADC(Pin(26))
 vout_pin = ADC(Pin(28))
@@ -11,6 +55,7 @@ pwm_en = Pin(1, Pin.OUT)
 pid = PID(0.15, 11, 0, setpoint=0.19, scale='ms')
 #pidvout = PID(0.2, 10, 0, setpoint= 3, scale='ms')
 
+
 count = 0
 c2 = 0
 elapsedtime = 0
@@ -19,9 +64,11 @@ pwm_ref = 0
 initsetpoint = pid.setpoint
 setpoint = pid.setpoint
 delta = 0.01
-
 demand = 0
-pid.setpoint = demand / 5
+
+
+
+
 
 global SHUNT_OHMS
 SHUNT_OHMS = 1.02
@@ -38,6 +85,18 @@ with open("LED_power.txt", "w") as file:
     file.write("Time (ms), Power (W)\n")
 
 while True:
+    data = None
+    ip = '192.168.194.92'
+    url = 'http:'+ip+':5000/demand'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        print('Failed to retrieve data from server')
+
+
+    demand = float(data['demand'])
+    pid.setpoint = demand / 5
     
     pwm_en.value(1)
 
