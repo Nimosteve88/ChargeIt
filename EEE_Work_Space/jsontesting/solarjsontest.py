12ff6add9ad6dfa3efcd6345cfa00aa7b3981e71
Nimosteve88 <nimosteve2@gmail.com>
2024-06-10 14:07:26 +0100

    Updates to Separate Smart Grid Communication

---
+++ EEE_Work_Space/jsontesting/solarjsontest.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Store the data in a global variable
+data = {}
+
+@app.route('/', methods=['POST'])
+def receive_data():
+    global data
+    data = request.get_json()
+    # Notify all clients about the updated data
+    socketio.emit('update_data', data)
+    return '', 204  # Return a 204 No Content response
+
+@app.route('/', methods=['GET'])
+def send_data():
+    return jsonify(data)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
